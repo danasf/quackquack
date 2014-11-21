8f39d6e8e2e44ced881f8537b1a4d9055496871c
Dana <dsniezko@gmail.com>
2014-11-21 10:23:58 -0800

    minor update

--- pyclient/mqtt_client.py
+++ pyclient/mqtt_client.py
@@ -14,13 +14,13 @@ gpio.output(led_pin,False)
  
 def on_connect(client, userdata, flags, rc):
 	print("Connected with result code "+str(rc))
-	client.subscribe("ducks/")
+	client.subscribe(sub_channel)
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
 	print(msg.topic+" "+str(msg.payload))
 	gpio.output(led_pin,True)
-	os.system('mplayer ~/quackquack/www/sounds/quack1.mp3 > /dev/null 2>&1 &')
+	os.system('mplayer /home/pi/quackquack/www/sounds/quack1.mp3 > /dev/null 2>&1 &')
 	sleep(0.2)
 	gpio.output(led_pin,False)
 
@@ -29,6 +29,6 @@ if __name__ == "__main__":
 	client = mqtt.Client()
 	client.on_connect = on_connect
 	client.on_message = on_message
-	client.connect("test.mosquitto.org", 1883, 60)
+	client.connect(host, 1883, 60)
 	while True:
 		client.loop()
